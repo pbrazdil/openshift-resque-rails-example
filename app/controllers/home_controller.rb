6017d8509c1d49e89059c32905dbbfa25f2c96c0
Vagrant <vagrant@example.com>
2013-08-13 02:51:15 -0700

    added number of jobs in queue

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
     Resque.enqueue(ExampleJob, "some-cool-parameter")
-    render :text => "Job inserted! These jobs are remaining #{Resque.peek(:main, 0, 100).to_yaml}"
+    render :text => "Job inserted! Jobs currently in the queue waiting to process: #{Resque.peek(:resize, 0, 100).size} "
   end
+
 end
