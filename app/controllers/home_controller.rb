0db51060912a0ab59c84e8974f445bc71c8c069a
Vagrant <vagrant@example.com>
2013-08-13 02:53:58 -0700

    fixed wrong queue name

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     Resque.enqueue(ExampleJob, "some-cool-parameter")
-    render :text => "Job inserted! Jobs currently in the queue waiting to process: #{Resque.peek(:resize, 0, 100).size} "
+    render :text => "Job inserted! Jobs currently in the queue waiting to process: #{Resque.peek(:main, 0, 1000).size} "
   end
 
 end
